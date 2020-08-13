e9a9471f568ba47b098b0faaa723521e889a6f4a
Wario <37343942+IfreannMedia@users.noreply.github.com>
2020-08-13 20:47:42 +0200

    added correct date display
    and removed unnessecary code

--- src/app/services/project-service.ts.service.ts
+++ src/app/services/project-service.ts.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { Project } from '../classes/project';
+import { NodeWithI18n } from '@angular/compiler';
 
 export enum technicalSkills {
   UX = "UX",
@@ -46,6 +47,16 @@ export enum bachalorSkills {
   germanCult = "German cultural studies",
 }
 
+export enum appleSkills {
+  systematicTroubleshooting = "systematic troubleshotting",
+  communication = "critical thinking",
+  problemSolving = "problem solving",
+  german = "german",
+  teamwork = "teamwork",
+  performanceTracking = "performance tracking",
+  goingAboveAndBeyondExp = "going above and beyond expectations"
+}
+
 export enum projectTypes {
   experience = 'experience',
   education = 'education',
@@ -58,112 +69,91 @@ export enum projectTypes {
 })
 export class ProjectService {
 
-  public projects: Project[] = []
+  public projects: Project[] = [{
+    name: 'Akka Technologies',
+    challengesAndOps: '',
+    description: 'At Akka I work as part of a determined dev team in the maintanance of a cross platform mobile first web App, used wordlwide in' +
+      ' production by a large autombile manufacturing company based in Munich. As well as maintaining and improving legacy code, I also add new features and was part of a total' +
+      ' migration of the angular and ionic versions.',
+    dateFrom: new Date('2019-04-01'),
+    dateTo: new Date(Date.now()),
+    projectType: projectTypes.experience,
+    technologiesUsed: [technicalSkills.Angular,
+    technicalSkills.Ionic,
+    technicalSkills.scss,
+    technicalSkills.Typescript,
+    technicalSkills.rxjs,
+    technicalSkills.crossPlaatformDev,
+    technicalSkills.Html,
+    technicalSkills.bitBucket,
+    technicalSkills.sourceTree,
+    technicalSkills.Jasmine,
+    technicalSkills.Sonarwube,
+    technicalSkills.UX,
+    technicalSkills.xCode,
+    technicalSkills.jira]
+  },
+  {
+    name: 'Creative Digital Media Msc',
+    challengesAndOps: '',
+    description: 'I was awarded a master\'s degree in Creative Digital Media with a specialization in game development in early 2019' +
+      ' from the Technological University of Dublin.',
+    dateFrom: new Date('2017-09-01'),
+    dateTo: new Date('2019-03-01'),
+    projectType: projectTypes.education,
+    technologiesUsed: [
+      technicalSkills.Html,
+      technicalSkills.Javascript,
+      technicalSkills.css,
+      technicalSkills.Unity,
+      technicalSkills.CSharp,
+      technicalSkills.Illustrator,
+      technicalSkills.PremierPro,
+      technicalSkills.Photoshop,
+      technicalSkills.UserTesting,
+      technicalSkills.PaperPrototyping,
+      technicalSkills.RapidPrototyping]
+  },
+  {
+    name: 'English, German BA International',
+    challengesAndOps: '',
+    description: 'During my 4 year BA International Joint Honors degree at Maynooth University and Universität Bielefeld, ' +
+      ' I enjoyed the pleasure of studing a variety of disciplines from literary and culteral analysis to statistics and neuroscience and even linguistic theory.' +
+      ' this time laid the foundations for my further work and studies.',
+    dateFrom: new Date('2011'),
+    dateTo: new Date('2015'),
+    projectType: projectTypes.education,
+    technologiesUsed: [
+      bachalorSkills.critThinking,
+      bachalorSkills.academicWriting,
+      bachalorSkills.litAnalysis,
+      bachalorSkills.lingTheory,
+      bachalorSkills.hLangDev,
+      bachalorSkills.neuroscience,
+      bachalorSkills.devPsychology,
+      bachalorSkills.socPsychology,
+      bachalorSkills.statistics,
+      bachalorSkills.scientificExperimentation,
+      bachalorSkills.germanCult]
+  }
+    , {
+    name: 'ApplecCare Technical Support',
+    challengesAndOps: '',
+    description: '',
+    dateFrom: new Date(),
+    dateTo: new Date(),
+    projectType: projectTypes.experience,
+    technologiesUsed: [
+      appleSkills.systematicTroubleshooting,
+      appleSkills.communication,
+      appleSkills.problemSolving,
+      appleSkills.german,
+      appleSkills.teamwork,
+      appleSkills.performanceTracking,
+      appleSkills.goingAboveAndBeyondExp,
+    ]
+  }];
 
   constructor() {
-    this.getProjects();
-  }
-
-  // TODO store the projects in a database and retrieve with a rest call
-  private getProjects() {
-    this.retrieveAkka();
-    this.retrieveMastersDegree();
-    this.retrieveAppleCare();
-    this.retrieveBachelorDegree();
-  }
-
-  public retrieveMastersDegree() {
-    let mastersDegree = new Project();
-    mastersDegree.name = "Creative Digital Media Msc";
-    mastersDegree.projectType = projectTypes.education;
-    if (this.projects.findIndex(proj => proj.name.toLowerCase() === mastersDegree.name.toLowerCase()) === -1) {
-      mastersDegree.technologiesUsed = [
-        technicalSkills.Html,
-        technicalSkills.Javascript,
-        technicalSkills.css,
-        technicalSkills.Unity,
-        technicalSkills.CSharp,
-        technicalSkills.Illustrator,
-        technicalSkills.PremierPro,
-        technicalSkills.Photoshop,
-        technicalSkills.UserTesting,
-        technicalSkills.PaperPrototyping,
-        technicalSkills.RapidPrototyping];
-      mastersDegree.description = "I was awarded a master's degree in Creative Digital Media with a specialization in game development in early 2019" +
-        " from the Technological University of Dublin."
-
-      this.projects.push(mastersDegree);
-    }
-    return mastersDegree;
-  }
-  public retrieveAkka() {
-    let mastersDegree = new Project();
-    mastersDegree.name = "Akka Technologies";
-    mastersDegree.projectType = projectTypes.experience;
-    mastersDegree.technologiesUsed = [
-      technicalSkills.Angular,
-      technicalSkills.Ionic,
-      technicalSkills.scss,
-      technicalSkills.Typescript,
-      technicalSkills.rxjs,
-      technicalSkills.crossPlaatformDev,
-      technicalSkills.Html,
-      technicalSkills.bitBucket,
-      technicalSkills.sourceTree,
-      technicalSkills.Jasmine,
-      technicalSkills.Sonarwube,
-      technicalSkills.UX,
-      technicalSkills.xCode,
-      technicalSkills.jira,
-    ];
-    mastersDegree.description = "At Akka I work as part of a determined dev team in the maintanance of a cross platform mobile first web App, used wordlwide in" +
-      " production by a large autombile manufacturing company based in Munich. As well as maintaining and improving legacy code, I also add new features and was part of a total" +
-      " migration of the angular and ionic versions.";
-
-    this.projects.push(mastersDegree);
-  }
-  public retrieveBachelorDegree() {
-    let mastersDegree = new Project();
-    mastersDegree.name = "English, German BA International";
-    mastersDegree.projectType = projectTypes.education;
-    mastersDegree.technologiesUsed = [
-      bachalorSkills.critThinking,
-      bachalorSkills.academicWriting,
-      bachalorSkills.litAnalysis,
-      bachalorSkills.lingTheory,
-      bachalorSkills.hLangDev,
-      bachalorSkills.neuroscience,
-      bachalorSkills.devPsychology,
-      bachalorSkills.socPsychology,
-      bachalorSkills.statistics,
-      bachalorSkills.scientificExperimentation,
-      bachalorSkills.germanCult];
-    mastersDegree.description = "During my 4 year BA International Joint Honors degree at Maynooth University and Universität Bielefeld, " +
-      " I enjoyed the pleasure of studing a variety of disciplines from literary and culteral analysis to statistics and neuroscience and even linguistic theory." +
-      " this time laid the foundations for my further work and studies.";
-
-    this.projects.push(mastersDegree);
-  }
-  public retrieveAppleCare() {
-    let mastersDegree = new Project();
-    mastersDegree.name = "ApplecCare Technical Support";
-    mastersDegree.projectType = projectTypes.experience;
-    mastersDegree.technologiesUsed = [
-      bachalorSkills.critThinking,
-      bachalorSkills.academicWriting,
-      bachalorSkills.litAnalysis,
-      bachalorSkills.lingTheory,
-      bachalorSkills.hLangDev,
-      bachalorSkills.neuroscience,
-      bachalorSkills.devPsychology,
-      bachalorSkills.socPsychology,
-      bachalorSkills.statistics,
-      bachalorSkills.scientificExperimentation,
-      bachalorSkills.germanCult];
-    mastersDegree.description = "Throughout my 2 years working at AppleCare at the European Headquarters in Cork, I enjoyed applying my linguistic, soft, and critical thinking " +
-      " skills in a fast-paced environment, working initially as a junior and then progressing to a senior support specialist." +
-      " my time here taught me an immense amount on customer relations, soft skills in general, and always going the extra mile.";
-
-    this.projects.push(mastersDegree);
   }
 }
