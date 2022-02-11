800d19d678d591a2dd027127852af7a34850d05e
Wario <37343942+IfreannMedia@users.noreply.github.com>
2022-02-11 17:41:43 +0100

    reorganized components and added jasmine type

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -1,10 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomePageComponent } from '../home-page/home-page.component';
-import { LandingBlockComponent } from '../landing-block/landing-block.component';
+import { LandingBlockComponent } from './landing-block/landing-block.component';
 import { ArtisticElementsComponent } from '../artistic-elements/artistic-elements.component';
 import { ExperienceHighlightsComponent } from './experience-highlights/experience-highlights.component';
-import { ProjectHighlightsComponent } from '../project-highlights/project-highlights.component';
+import { ProjectHighlightsComponent } from '../components/project-highlights/project-highlights.component';
 
 
 
