24a7f0b613b258039d3c242f35ed27afdeeb3fd2
Wario <37343942+IfreannMedia@users.noreply.github.com>
2022-02-12 16:54:51 +0100

    added drop shadow on scroll
    new nav links and qualifications

--- src/app/components/nav-bar/nav-bar.component.ts
+++ src/app/components/nav-bar/nav-bar.component.ts
@@ -1,19 +1,42 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, HostListener, OnDestroy, OnInit, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-nav-bar',
   templateUrl: './nav-bar.component.html',
   styleUrls: ['./nav-bar.component.scss']
 })
-export class NavBarComponent implements OnInit {
+export class NavBarComponent implements OnInit, OnDestroy {
 
-  constructor() { }
+  @ViewChild('navbar') private navbar!: ElementRef;
 
-  ngOnInit(): void {
+  @HostListener('window:scroll', ['$event'])
+  handleScrollEvent(event: Event) {
+    this.addStyling(event);
   }
 
+  constructor() { }
+  ngOnInit() {
 
+  }
+
+  ngOnDestroy() {
+    window.removeEventListener('scroll', this.addStyling, true);
+  }
   public navigateTo(url: string): void {
     window.open(url, "_blank");
+  }
+
+  private addStyling(event: Event): void {
+    console.log(event);
+    const scrollingEl: Element | null = (event.target as Document).scrollingElement;
+    if (!this.navbar || (!event || !scrollingEl))
+      return;
+    const scrollTopVal: number = scrollingEl.scrollTop;
+    if (scrollTopVal > 0) {
+      debugger;
+      this.navbar.nativeElement.classList.add("shadow");
+    } else {
+      this.navbar.nativeElement.classList.remove("shadow");
+    }
   }
 }
