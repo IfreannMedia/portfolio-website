f4e550998871e95b65434ddae076ac3ea4e8c333
Wario <37343942+IfreannMedia@users.noreply.github.com>
2022-02-27 13:24:17 +0100

    update with headers, logging

--- src/app/services/home.service.ts
+++ src/app/services/home.service.ts
@@ -2,7 +2,7 @@ import { Experience } from './../classes/experience';
 import { Project } from './../classes/project';
 import { Injectable } from '@angular/core';
 import { environment } from '../../environments/environment';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { first } from 'rxjs/operators';
 
@@ -14,8 +14,10 @@ export class HomeService {
   constructor(private http: HttpClient) { }
 
   public getProjects(): Observable<Project[]> {
+    let headers: HttpHeaders = new HttpHeaders();
     console.log("building URL, prod:  ", environment.production, " ", environment.baseUrl)
-    return this.http.get<Project[]>(environment.baseUrl + "projects").pipe(first());
+    console.log("with headers:", headers)
+    return this.http.get<Project[]>(environment.baseUrl + "projects", {headers: headers}).pipe(first());
   }
 
   public getExperiences(): Observable<Experience[]> {
